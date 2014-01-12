758e982eb0018c443744df722a785ba3adda661c
kemaldarmawan <kemal30darmawan@gmail.com>
2014-01-12 18:33:50 +0700

    general change

--- DanaiBeta/src/main/java/com/danai/app/HomeController.java
+++ DanaiBeta/src/main/java/com/danai/app/HomeController.java
@@ -44,72 +44,15 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Map<String, Object> map) {
-		//map.put("user",userDao.getUser(1));
-		map.put("userList",userDao.getAllUser());
-		map.put("locationList", locationDao.getAllLocation());
-		map.put("categoryList", categoryDao.getAllCategory());
-		map.put("projectList", projectDao.getAllProject() );
-		//map.put("createdProjectList", userDao.getUser(1).getCreatedProject() );
-		//map.put("fundProjectList", projectDao.getProject(2).getFunds() );
-		//map.put("commentProjectList", projectDao.getProject(2).getComments() );
+		
 		return "danai";
 	}
 	
-	
-	@RequestMapping(value = "/list/user", method = RequestMethod.GET)
-	public String listUser(Model model){
-		
-		model.addAttribute("listUser",userDao.getAllUser());
-		return "home";
+	@RequestMapping(value = "/search",method = RequestMethod.GET)
+	public String search(Model model){
+		return "search";
 	}
 	
-	@RequestMapping(value="/model_testing", method = RequestMethod.GET)
-	public String modelTesting(Model model)
-	{
-		model.addAttribute("user",userDao.getUser(1));
-		model.addAttribute("userList",userDao.getAllUser());
-		model.addAttribute("locationList", locationDao.getAllLocation());
-		model.addAttribute("locationProjectList", locationDao.getLocation(1).getProjects());
-		model.addAttribute("categoryList", categoryDao.getAllCategory());
-		model.addAttribute("categoryProjectList", categoryDao.getCategory(1).getProjects());
-		model.addAttribute("projectList", projectDao.getAllProject() );
-		model.addAttribute("createdProjectList", userDao.getUser(1).getCreatedProject() );
-		model.addAttribute("fundProjectList", projectDao.getProject(2).getFunds() );
-		model.addAttribute("commentProjectList", projectDao.getProject(2).getComments() );
-		return "home";
-		
-	}
-	
-	@RequestMapping(value="/model_testing_edit_del", method = RequestMethod.GET)
-	public String modelTestingEdDel(Model model, HttpServletRequest request)
-	{
-		// edit location
-		/*int loc_id = Integer.parseInt(request.getParameter("id"));
-		String new_name = request.getParameter("city");
-		Location loc = locationDao.getLocation(loc_id);
-		loc.setCity(new_name);
-		locationDao.edit(loc);*/
-		// add category
-		/*String name = request.getParameter("name");
-		Category cat = new Category();
-		cat.setName(name);
-		categoryDao.add(cat);*/
-		// del location
-		int loc_id = Integer.parseInt(request.getParameter("id"));
-		locationDao.delete(loc_id);
-		model.addAttribute("user",userDao.getUser(1));
-		model.addAttribute("userList",userDao.getAllUser());
-		model.addAttribute("locationList", locationDao.getAllLocation());
-		model.addAttribute("locationProjectList", locationDao.getLocation(1).getProjects());
-		model.addAttribute("categoryList", categoryDao.getAllCategory());
-		model.addAttribute("categoryProjectList", categoryDao.getCategory(1).getProjects());
-		model.addAttribute("projectList", projectDao.getAllProject() );
-		model.addAttribute("createdProjectList", userDao.getUser(1).getCreatedProject() );
-		model.addAttribute("fundProjectList", projectDao.getProject(2).getFunds() );
-		model.addAttribute("commentProjectList", projectDao.getProject(2).getComments() );
-		return "home";
-		
-	}
 	
 	@RequestMapping(value="/user.do", method=RequestMethod.POST)
 	public String doActions(@ModelAttribute User user, BindingResult result, @RequestParam String action, Map<String, Object> map) {
